89f61fba3844701b19788b37301f197edc680844
dilsenyildar <dilsenyildar@galaksiya.com>
2020-05-02 22:57:34 +0300

    stop words implemented

--- NlpApi.py
+++ NlpApi.py
@@ -20,9 +20,21 @@ def initialize_nlp_process(data):
     verbs_json = []
     entity_json = []
 
-    doc = process_data(data)
+    nlp = spacy.load("en_core_web_lg")
+    doc = process_data(data, nlp)
+    all_stopwords = nlp.Defaults.stop_words
+    all_stopwords.remove("the")
+
     for noun in doc.noun_chunks:
-        nouns_json.append(noun.text.lower())
+        nounWords = noun.text.split()
+        for nw in nounWords:
+            for word in all_stopwords:
+                if nw == word:
+                    nounWords.remove(nw)
+                    print("removed stop words:" + nw)
+        text = ' '.join(nounWords)
+        if len(text) > 0:
+            nouns_json.append(text.lower())
     for token in doc:
         if token.pos_ == "VERB":
             verbs_json.append(token.lemma_.lower())
@@ -31,8 +43,7 @@ def initialize_nlp_process(data):
     return jsonify({"NounPhrases": nouns_json, "Verbs": verbs_json, "Entities": entity_json})
 
 
-def process_data(data):
-    nlp = spacy.load("en_core_web_lg")
+def process_data(data, nlp):
     request_json = json.loads(data)
     text = request_json['document']
     return nlp(text)
